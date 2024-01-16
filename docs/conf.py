71f05ea56f3fa71284354eda630ea87de7aa20ff
Annie Gray <annie-m-gray@hotmail.co.uk>
2024-01-16 11:38:48 +0000

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,14 +10,18 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'RelaStat'
-copyright = '2023, Annie Gray'
-author = 'Annie Gray'
+project = 'relastat'
+copyright = '2024, Annie'
+author = 'Annie'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
